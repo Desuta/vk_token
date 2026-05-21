604ffcf88da6a473b2fafd0240da1e90e61d5bb0
Desuta <zayavlenskih.dimon@gmail.com>
2026-05-21 19:22:28 +0500

    Alpha

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const apps = [
 
         [
-6463690, 'Маруся', 'https://sun9-56.userapi.com/Zbl1njzBDZ4v9shXbclysDxLjeG7KHgYFAlwMw/rb-hbmdVyxI.jpg', 'ВКонтакте для Android', 132]
+6463690, 'Маруся', 'https://sun9-56.userapi.com/Zbl1njzBDZ4v9shXbclysDxLjeG7KHgYFAlwMw/rb-hbmdVyxI.jpg', 'Умная колонка от VK', 132]
     ],
     utypes = [
         [0, 'Уведомления', 'notify', 'только для iframe-приложений'],
